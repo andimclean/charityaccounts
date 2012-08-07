34abf90d5002a61a2a42b29dc6b9e25645b61f59
libra guest <andi_mclean@ntlworld.com>
2012-08-07 15:31:24 -0400

    Hello world test

---
+++ bin/server.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Dragon\n');
+}).listen(process.env.PORT || 80, process.env.IP || '0.0.0.0');
