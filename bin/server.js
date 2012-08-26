6514aa9ab69d36e3af1b9971abc196475de1879e
Andi McLean <andi_mclean@ntlworld.com>
2012-08-26 16:49:00 +0100

    Got it working on jitsu

--- bin/server.js
+++ bin/server.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Dragon\n');
-}).listen(process.env.PORT || 80, process.env.IP || '0.0.0.0');
+}).listen(parseInt(process.env.PORT) || 80, process.env.IP || '0.0.0.0');
